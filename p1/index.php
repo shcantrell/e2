fe8f7d647cc8658d908454416fa61f2310a69098
Susan Cantrell <scantrell@g.harvard.edu>
2019-09-17 19:45:01 -0400

    Corrected HTML after Validation

--- p1/index.php
+++ p1/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
 <head>
     <title>Susan H. Cantrell</title>
     <meta charset='utf-8'>
@@ -42,20 +42,19 @@
 <body>
     <div class='container'>
         <img src="/images/shcantrell_headshot_small.jpg" alt="Headshot of Susan Cantrell at the National Mall in Washington, DC.">
-
         <h1>Susan H. Cantrell</h1>
         <aside>
-        <p>Hello! This will be my last HES course before tackling the capstone project (eeeek!) in pursuit of a Masters of Liberal Arts in Digital Media Design.</p>
+            <p>Hello! This will be my last HES course before tackling the capstone project (eeeek!) in pursuit of a Masters of Liberal Arts in Digital Media Design.</p>
 
-        <p>I started as a graphic designer over 15 years ago, which only evolved into web design and is now inching toward web development - what that last part means exactly, I am still figuring out.</p>
-                
-        <P>The bulk of my career involved working in the public sector, making it no coincidence that I currently work within IT at the Mass. Executive Office of Health and Human Services (EOHHS) as IT's Communications Director.</p>
+            <p>I started as a graphic designer over 15 years ago, which only evolved into web design and is now inching toward web development - what that last part means exactly, I am still figuring out.</p>
+                    
+            <P>The bulk of my career involved working in the public sector, making it no coincidence that I currently work within IT at the Mass. Executive Office of Health and Human Services (EOHHS) as IT's Communications Director.</p>
 
-        <p>At EOHHS, we dove in headfirst with the goal of a new IT intranet presence using Drupal (one of the most popoular content management systems for the government sector). Unknowingly, we stood-up and developed a Drupal 7 site on a Windows environment, realized our mistake and migrated to an in-house Linux environment (stood-up our own LAMP stack), only to start over in Drupal 8 when the project was upgraded from just an IT intranet site to an intranet site for the entirety of the Secretariat. You have no idea how desperate we are to figure out how to setup a GIT version control workflow on our local desktops!</p>
+            <p>At EOHHS, we dove in headfirst with the goal of a new IT intranet presence using Drupal (one of the most popoular content management systems for the government sector). Unknowingly, we stood-up and developed a Drupal 7 site on a Windows environment, realized our mistake and migrated to an in-house Linux environment (stood-up our own LAMP stack), only to start over in Drupal 8 when the project was upgraded from just an IT intranet site to an intranet site for the entirety of the Secretariat. You have no idea how desperate we are to figure out how to setup a GIT version control workflow on our local desktops!</p>
 
-        <p>So, that leads me here...to "WEB PROGRAMMING FOR BEGINNERS WITH PHP"- the backbone of Drupal (PHP, that is). I realize that Drupal will not be addressed in this course, but I know I am going to have a better understanding of core concepts that Drupal's programming is based on.</p>
+            <p>So, that leads me here...to "WEB PROGRAMMING FOR BEGINNERS WITH PHP"- the backbone of Drupal (PHP, that is). I realize that Drupal will not be addressed in this course, but I know I am going to have a better understanding of core concepts that Drupal's programming is based on.</p>
 
-        <p>I am quite sincere when I say, I look forward to the class.</p>
+            <p>I am quite sincere when I say, I look forward to the class.</p>
         </aside>
     </div>
 </body>
