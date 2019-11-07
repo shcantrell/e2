285122728242cfe945ea6086c532a9dca1c4e26e
Susan Cantrell <scantrell@g.harvard.edu>
2019-11-06 23:33:39 -0500

    Upload ZipFoods 'About' Page

--- zipfoods/routes.php
+++ zipfoods/routes.php
@@ -2,4 +2,7 @@
 
 return [
     '/' => ['AppController', 'index'],
+    '/about' => ['AppController', 'about'],
+    '/contact' => ['AppController', 'contact'],
+    '/products' => ['ProductController', 'index'],
 ];
