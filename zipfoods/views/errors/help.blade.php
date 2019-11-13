b54c5e329d61d251b890b35eb48e0ae8e7666ced
Susan Cantrell <scantrell@g.harvard.edu>
2019-11-13 00:24:30 -0500

    Might as well be late to add the 404 page

--- zipfoods/views/errors/help.blade.php
+++ zipfoods/views/errors/help.blade.php
@@ -1,15 +1,15 @@
 @extends('templates.master')
 
 @section('title')
-Error 404
+Product Not Found
 @endsection
 
 @section('content')
 <div id='product-show'>
-    <h2>ERROR 404</h2>
+    <h2>Product 99 not found</h2>
 
-    <p>The option chosen did not have any results.</p>
-    <a href='/products'>&larr; Return to all products</a>
+    <p>Uh oh - we were not able to find the product you were looking for.</p>
+    <a href='/products'>Check out our other prodcuts...</a>
 
 
     @endsection
